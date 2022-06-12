4faddb005c90eed98798e2744ee0d2d17105a82c
trinibucc <tbucciarelli@hotmail.com>
2022-06-12 15:33:27 -0300

    Modifico comparacion de actualizdor

--- estadio/Actualizador.go
+++ estadio/Actualizador.go
@@ -5,10 +5,10 @@ type Actualizador struct {
 
 func (actualizador *Actualizador) ActualizarEstado(disponible Disponible, entradasDisponibles int64) {
 
-	if entradasDisponibles <= 0 {
-		disponible.seccion.ModificarCapacidad(0, Agotado{disponible.seccion})
-	} else {
+	if entradasDisponibles > 0 {
 		disponible.seccion.ModificarCapacidad(entradasDisponibles, disponible)
+	} else {
+		disponible.seccion.ModificarCapacidad(0, Agotado{disponible.seccion})
 	}
 
 }
